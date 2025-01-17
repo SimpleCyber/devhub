9d42643101e003856d76e2ec658967f2c6307836
Satyam Yadav <satyamok03@gmail.com>
2025-01-17 14:44:55 +0530

    deploy frontend updated the urls

--- frontend/src/components/api/linkedin.js
+++ frontend/src/components/api/linkedin.js
@@ -8,7 +8,7 @@ function LinkedinProfile() {
 
     const fetchLinkedInData = async () => {
         try {
-            const response = await axios.get(`http://127.0.0.1:8000/api/linkedin/${username}/`);
+            const response = await axios.get(`https://devhub-k9dg.onrender.com/api/linkedin/${username}/`);
             setData(response.data);
             setError(""); // Clear any previous error
         } catch (err) {
